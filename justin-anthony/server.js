e5c84d2e39c2164d02234dfe87f5eb25212bd64e
Anthony Green <Anthonydgreen90@gmail.com>
2018-04-18 10:06:12 -0700

    created new express path for new.html

--- justin-anthony/server.js
+++ justin-anthony/server.js
@@ -12,6 +12,10 @@ app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}!`);
 } );
 
+app.get('/new',(request,response) => {
+  response.sendFile('new.html',{root: './public'});
+});
+
 // REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
 app.post('/articles', express.urlencoded(), function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
