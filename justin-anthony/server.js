81fcb7b5ba545b0bac089b3793a6f1f96fc28261
Anthony Green <Anthonydgreen90@gmail.com>
2018-04-18 10:10:11 -0700

    added 404 error message

--- justin-anthony/server.js
+++ justin-anthony/server.js
@@ -16,6 +16,10 @@ app.get('/new',(request,response) => {
   response.sendFile('new.html',{root: './public'});
 });
 
+app.use((req, res) => {
+  res.status(404).send('Sorry, that route does not exist');
+});
+
 // REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
 app.post('/articles', express.urlencoded(), function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
